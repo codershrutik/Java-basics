19213c5fb515562995b87d95adddaf4e0b92feae
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-14 04:49:58 +0000

    Mod added

--- decisions/arithmatic.java
+++ decisions/arithmatic.java
@@ -2,5 +2,8 @@ public class arithmatic{
     public static void main(String[] args) {
         int x = 10*11;
         System.out.println(x);
+
+        int mod = 13%2;
+        System.out.println(mod);
     }
 }
